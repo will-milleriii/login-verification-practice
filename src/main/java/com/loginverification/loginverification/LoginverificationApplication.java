872d1063c2a5d364442db2b8d20a73d159f6166b
Will Miller <willmiller629@gmail.com>
2021-02-01 10:33:59 -0500

    initial commit

---
+++ src/main/java/com/loginverification/loginverification/LoginverificationApplication.java
@@ -0,0 +1,13 @@
+package com.loginverification.loginverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginverificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginverificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loginverification/loginverification/LoginverificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginverification.loginverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginverificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
